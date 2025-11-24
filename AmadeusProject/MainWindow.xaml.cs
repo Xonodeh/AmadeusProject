6f9b0b9d663453aa04ef016a2d465432aadf2561
nael <haddadi.nael@gmail.com>
2025-11-24 17:11:40 +0100

    ajout interface de chargement XAML, et code de la classe SpriteManager + méthodes

--- AmadeusProject/MainWindow.xaml.cs
+++ AmadeusProject/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
+﻿using System;
+using System.IO; // Nécessaire pour vérifier si le fichier existe
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Imaging; // Nécessaire pour gérer les Bitmaps
+using AmadeusProject.Services;
 
 namespace AmadeusProject
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        SpriteManager _spriteManager;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _spriteManager = new SpriteManager();
+
+        
+            string initialPath = _spriteManager.GetSpritePath("[HAPPY]");
+
+            if (File.Exists(initialPath))
+            {
+                KurisuDisplay.Source = new BitmapImage(new Uri(initialPath, UriKind.Absolute));
+            }
+            else
+            {
+                MessageBox.Show($"Impossible de trouver l'image ici :\n{initialPath}\n\nVérifie que tes images sont bien dans le dossier 'Assets' et en mode 'Copier si plus récent'.");
+            }
+
+            this.MouseDown += (s, e) => { if (e.LeftButton == MouseButtonState.Pressed) DragMove(); };
         }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e) { }
+        private void UserInput_KeyDown(object sender, KeyEventArgs e) { }
     }
 }
--- AmadeusProject/Services/SpriteManager.cs
+++ AmadeusProject/Services/SpriteManager.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace AmadeusProject.Services
 {
-    class SpriteManager
+    public class SpriteManager
     {
+        private Dictionary<string, string> _sprites;
+
+        public SpriteManager()
+        {
+            _sprites = new Dictionary<string, string>
+            {
+                { "[NEUTRAL]",   "peaceful.png" },
+                { "[HAPPY]",     "happy.png" },
+                { "[VERYHAPPY]", "veryhappy.png" },
+                { "[ANGRY]",     "angry.png" },
+                { "[VERYANGRY]", "veryangry.png" },
+                { "[SAD]",       "sad.png" },
+                { "[SHY]",       "shy.png" },
+                { "[VERYSHY]",   "veryshy.png" },
+                { "[SMIRK]",     "smirk.png" }
+            };
+        }
+
+        public string GetSpritePath(string emotionTag)
+        {
+            string key = emotionTag.Trim().ToUpper();
+
+            string fileName;
+            if (_sprites.ContainsKey(key))
+            {
+                fileName = _sprites[key];
+            }
+            else
+            {
+                fileName = "happy.png"; // Image par défaut
+            }
+
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", fileName);
+        }
     }
 }
